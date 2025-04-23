2930969fce3c47679b162ef0f669e19eb1e1b6d0
leeJS <dlwnstjzld@gmail.com>
2025-04-23 22:08:11 +0900

    set Search component layout

--- app/component/company/Search.tsx
+++ app/component/company/Search.tsx
@@ -1,7 +1,31 @@
-import React, { FC } from "react";
+"use client";
 
-const Search: FC = () => {
-  return <div>Search</div>;
-};
+import React, { useRef, useState } from "react";
 
-export default Search;
+export function Search() {
+  //const [searchBarValue, setSearchBarValue] = useState<String>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const removeButtonHanler = (): void => {
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div className="bg-gray-200 rounded-lg flex w-full md:max-w-md h-12 items-center">
+      <div className="w-1/8 flex justify-center">
+        <i className="ri-search-line"></i>
+      </div>
+      <div className="w-3/4">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Insert Here"
+          className="w-full focus:outline-none placeholder-gray-400"
+        />
+      </div>
+      <div className="w-1/8 flex justify-center" onClick={removeButtonHanler}>
+        <i className="ri-close-line"></i>
+      </div>
+    </div>
+  );
+}
