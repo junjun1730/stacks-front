6a58fcdb240aabc1abf5288412332d38fe627c8d
leeJS <dlwnstjzld@gmail.com>
2025-04-24 21:08:16 +0900

    change Company page flex gap

--- app/company/list/layout.tsx
+++ app/company/list/layout.tsx
@@ -7,5 +7,5 @@ export default function CompanyLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="flex flex-col gap-2">{children}</div>;
+  return <div className="flex flex-col gap-5">{children}</div>;
 }
