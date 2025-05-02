644ba4d5d0646b5bac08758e67411c54e4250d7d
leeJS <dlwnstjzld@gmail.com>
2025-05-02 17:26:51 +0900

    set PrimaryButton layout

--- app/component/common/PrimaryButton.tsx
+++ app/component/common/PrimaryButton.tsx
@@ -1,3 +1,9 @@
 export function PrimaryButton() {
-  return <div>PrimaryButton</div>;
+  return (
+    <div className="flex justify-end mt-2">
+      <button className="border-blue-500 bg-blue-400 cursor-pointer hover:bg-blue-50 hover:text-black duration-150 p-4 rounded-2xl font-bold text-white">
+        Submit
+      </button>
+    </div>
+  );
 }
