ffa13f386800758526765d639a532abbe0859c51
leeJS <dlwnstjzld@gmail.com>
2025-05-04 20:25:01 +0900

    不要ソース削除

--- app/component/company/CardList.tsx
+++ app/component/company/CardList.tsx
@@ -5,20 +5,6 @@ import { Card } from "./Card";
 export async function CardList() {
   const cardData: CardDataList = null;
 
-  // const [cardData, setCardData] = useState<CardDataList>(null);
-
-  // useEffect(() => {
-  //   async function getAllData() {
-  //     try {
-  //       const data: CardDataList = await getAllDummyData();
-  //       setCardData(data);
-  //     } catch (e) {
-  //       setCardData(null);
-  //     }
-  //   }
-  //   getAllData();
-  // }, []);
-
   return (
     <div>
       <Card cardData={cardData} />
